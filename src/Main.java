e213567be9312b75c6754e7b20ca68daecaf94b3
Shree Devi Yadam <shree.yadam@gmail.com>
2022-07-26 15:44:18 -0700

    Add another overload method

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         int newScore = calculateScore("SDY", 500);
         System.out.println("New score is " + newScore);
         calculateScore(75);
+        calculateScore();
     }
 
     public static int calculateScore(String playerName, int score) {
@@ -13,5 +14,10 @@ public class Main {
     public static int calculateScore(int score) {
         System.out.println("Unnamed player scored " + score + " points");
         return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("No player name, no player score.");
+        return 0;
     }
 }
