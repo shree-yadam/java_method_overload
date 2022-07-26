2e8d5e91d0a471e8daf39a18f1c9a98a8597de4c
Shree Devi Yadam <shree.yadam@gmail.com>
2022-07-26 15:57:56 -0700

    Calculate centimeters given feet and inches

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
         System.out.println("New score is " + newScore);
         calculateScore(75);
         calculateScore();
+        System.out.println(calcFeetAndInchesToCentimeters(1, 2));
+        System.out.println(calcFeetAndInchesToCentimeters(14));
     }
 
     public static int calculateScore(String playerName, int score) {
@@ -19,5 +21,24 @@ public class Main {
     public static int calculateScore() {
         System.out.println("No player name, no player score.");
         return 0;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(int feet, int inches) {
+        if(feet < 0 || inches < 0 || inches > 12) {
+            return -1;
+        }
+
+        double centimeters = (feet * 12 + inches) * 2.54;
+        return centimeters;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(int inches) {
+        if(inches < 0) {
+            return -1;
+        }
+
+        int feet = (inches - inches % 12)/ 12;
+        inches = inches % 12;
+        return calcFeetAndInchesToCentimeters(feet, inches);
     }
 }
